fa0960d51060bd8fed7d2a0967f4058e2366feb1
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-14 14:18:30 +0200

    durch

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('gallery').innerHTML = '<figure><img src="https://unsplash.it/400/400" alt="unsplash400"><figcaption>Fig. 1</figcaption></figure>';
+
+document.getElementById('gallery').innerHTML += '<figure><img src="https://unsplash.it/400/400" alt="unsplash400"><figcaption>Fig. 2</figcaption></figure>';
+
+document.getElementById('gallery').innerHTML += '<figure><img src="https://unsplash.it/400/400" alt="unsplash400"><figcaption>Fig. 3</figcaption></figure>';
